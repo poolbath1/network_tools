517d7f6ad292e472d7dd7eb2624af79706205afe
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-02-10 18:36:58 -0800

    echo server seems to be working

--- borrowed/echo-server.py
+++ borrowed/echo-server.py
@@ -10,18 +10,21 @@ def server_sock():
     server_socket.bind(('127.0.0.1', 8888))
     server_socket.listen(10)
 
-    conn, addr = server_socket.accept()
     buffsize = 4096
-    msg = ""
-    done = False
-    while not done:
-        part = conn.recv(buffsize)
-        if len(part) < buffsize:
-            done = True
-        msg = "{}{}".format(msg, part)
-    out = "{}{}".format("I heard: ", msg)
-    conn.sendall(out)
-
+    out = ""
+    while True:
+        msg = ""
+        conn, addr = server_socket.accept()
+        done = False
+        while not done:
+            part = conn.recv(buffsize)
+            print part
+            if len(part) < buffsize:
+                done = True
+            msg = "{}{}".format(msg, part)
+        out = msg
+        conn.sendall(out)
+        conn.close()
 
 if __name__ == '__main__':
     server_sock()
